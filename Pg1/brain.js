da613e49e9876b2a6d488f4578f62e907ef25ebe
Sayad Pervez <38353201+SayadPervez@users.noreply.github.com>
2020-07-16 17:34:18 +0530

    Update brain.js

--- Pg1/brain.js
+++ Pg1/brain.js
@@ -17,6 +17,7 @@ else
     images=['./bg3.jpg','./bg31.jpg'];
     document.getElementById("parent").style.backgroundImage = "url(" + images[Math.round(Math.random())] + ")";
 }
+	document.getElementById('test').innerHTML=navigator.userAgent;
 }
 
 function insta(){
