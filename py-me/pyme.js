6bf9fc7289225623dd5314bb5be39d15a117f512
Sayad Pervez <38353201+SayadPervez@users.noreply.github.com>
2021-05-05 14:10:42 +0530

    Create pyme.js

---
+++ py-me/pyme.js
@@ -0,0 +1,16 @@
+function getServer()
+{
+    var d1 = new Date();
+    var x=d1.toUTCString()
+    var num = Number(x.substring(5,7));
+    if(num<=15)
+    {
+        //console.log("https://py-me.herokuapp.com/"+"\n"+x);
+        return("https://py-me.herokuapp.com/");
+    }
+    else
+    {
+        //console.log("https://py-me-x.herokuapp.com/"+"\n"+x);
+        return("https://py-me-x.herokuapp.com/");
+    }
+};
